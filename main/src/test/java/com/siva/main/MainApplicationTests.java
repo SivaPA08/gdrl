394feeb14a65270d8d2507b9aacdcb8ffceae65c
Siva <sivaprasada2004@gmail.com>
2025-12-06 12:02:37 +0530

    added main file

---
+++ main/src/test/java/com/siva/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siva.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
